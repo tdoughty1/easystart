467b4a9e5f8cea0eb0dcad5bdf16f06176703b19
Todd Doughty <tdoughty1@berkeley.edu>
2016-01-10 15:27:01 -0800

    Renamed module files, matched to pep8 standard

--- easystart.py
+++ easystart.py
@@ -6,17 +6,19 @@ import os
 # Custom Modules
 import write_setup
 
+
 # Define a simple namespace class to hold config options
 class Config():
     pass
 
+
 def main():
-    
+
     # Create config object
     config = Config()
-    
-    ## Get input from user
-    
+
+    # Get input from user
+
     # Project Info
     config.project_name = raw_input('Input Project Name: ')
     config.author = raw_input('Author Name: ')
@@ -25,87 +27,87 @@ def main():
 
     # Check if user wants to use version control
     vcsToken = raw_input('Implement Version Control: ')
-    config.vcsFlag = vcsToken in ['1','T','True','t','true','y','Y','yes','Yes'] 
+    config.vcsFlag = vcsToken in ['1', 'T', 'True', 't', 'true', 'y', 'Y',
+                                  'yes', 'Yes']
 
     # Check which vcs
     if config.vcsFlag:
         print 'Currently there is only support for Git'
-        
-        #TODO Support Additional vcs options
-    
+
+        # TODO Support Additional vcs options
+
     # Check for hosting choice
     hostToken = raw_input('Do you want to host the code? ')
-    config.hostFlag = hostToken in ['1','y','Y','yes','Y']
-    
+    config.hostFlag = hostToken in ['1', 'y', 'Y', 'yes', 'Y']
+
     # Check which host
     if config.hostFlag:
         config.hostOpt = raw_input('Bitbucket (1) or Github (2)? ')
         config.hostID = raw_input('Username: ')
-        config.hostProject = raw_input('Project Repo name (' + project_name.lower() + '): ') 
-        print 'For the automated hosting option to function, the remote repository should already be initialized.'
-        
-        config.hostInit = raw_input('Has remote repository been created?') 
-    
-        
+        config.hostProject = raw_input('Project Repo name ('
+                                       + project_name.lower() + '): ')
+        print 'For the automated hosting option to function, the '
+        print 'remote repository should already be initialized.'
 
-    #TODO Add automatic license implementation
+        config.hostInit = raw_input('Has remote repository been created?')
+
+    # TODO Add automatic license implementation
     # License information
 
     # Create project repo
     os.mkdir(config.project_name)
-    
+
     # Move into repo
     os.chdir(config.project_name)
-    
+
     # Add Setup files
     write_setup.write_python(config)
-    
-    f = open('setup.cf','w')
+
+    f = open('setup.cf', 'w')
     f.write('')
     f.close()
 
     # Add Readme
-    f = open('README.rst','w')
+    f = open('README.rst', 'w')
     f.write('')
     f.close()
 
     # Add Manifest
-    f = open('MANIFEST.in','w')
+    f = open('MANIFEST.in', 'w')
     f.write('')
     f.close()
 
     # Add License
-    f = open('LICENSE','w')
+    f = open('LICENSE', 'w')
     f.write('')
     f.close()
-    
+
     # Add Todo
-    f = open('TODO.md','w')
+    f = open('TODO.md', 'w')
     f.write('')
     f.close()
-    
+
     # Add Requirements
-    f = open('requirements.txt','w')
+    f = open('requirements.txt', 'w')
     f.write('')
     f.close()
-    
+
     # Make docs directory
     os.mkdir('docs')
-    
+
     # Make tests directory
     os.mkdir('tests')
-    
+
     # Make project module folder
     os.mkdir(config.project_name.lower())
-    
+
     # Move into project module folder
     os.chdir(config.project_name.lower())
-    
+
     # Make into a module
-    f = open('__init__.py','w')
+    f = open('__init__.py', 'w')
     f.write('')
     f.close()
 
 if __name__ == '__main__':
     main()
-
--- writesetup.py
+++ writesetup.py
@@ -5,16 +5,20 @@ Module to automate the creation of setup files for the start project approach.
 
 """
 
+
 def write_python(config):
 
+    # TODO make optional based on config settings
+
     # Open file for writing
-    f = open('setup.py','w')
+    f = open('setup.py', 'w')
 
     # Module docstring
     f.write('#! /usr/bin/env python\n')
     f.write('\n')
     f.write('"""\n')
-    f.write('A setuptools based setup module for ' + config.project_name + '.\n')
+    f.write('A setuptools based setup module for ')
+    f.write(config.project_name + '.\n')
     f.write('Modeled from:\n')
     f.write('https://packaging.python.org/en/latest/distributing.html\n')
     f.write('https://github.com/pypa/sampleproject\n')
@@ -30,38 +34,46 @@ def write_python(config):
     # Get description from README
     f.write('# Get the long description from the README file\n')
     f.write('here = path.abspath(path.dirname(__file__))\n')
-    f.write("with open(path.join(here, 'README.rst'), encoding='utf-8') as f:\n")
+    f.write('with open(path.join(here, 'README.rst'),')
+    f.write(" encoding='utf-8') as f:\n")
     f.write('    long_description = f.read()\n\n')
-    
+
     # Include Setup configuration options
+    f.write('# Specificy Setup options\n')
     f.write('setup(\n')
     f.write('    name=' + config.project_name + ',\n\n')
 
     # Version info
-    f.write('    # Versions should comply with PEP440.  For a discussion on single-sourcing\n')
+    f.write('    # Versions should comply with PEP440.  For a discussion')
+    r.write(' on single-sourcing\n')
     f.write('    # the version across setup.py and the project code, see\n')
-    f.write('    # https://packaging.python.org/en/latest/single_source_version.html\n')
+    f.write('    # https://packaging.python.org/en/latest/')
+    f.write('single_source_version.html\n')
     f.write("    version=" + config.version + ",\n\n")
-    
+
+    # TODO include description in setup questions
     # Description info
     f.write("    description='A sample Python project',\n")
     f.write("    long_description=long_description,\n\n")
 
+    # TODO get host info
     # Hosting info
     f.write("    # The project's main homepage.\n")
     f.write("    url='https://github.com/pypa/sampleproject',\n\n")
 
     # Author info
     f.write("    # Author details\n")
-    f.write("    author='The Python Packaging Authority',\n")
-    f.write("    author_email='pypa-dev@googlegroups.com',\n\n")
+    f.write("    author='" + config.author + "',\n")
+    f.write("    author_email='" + config.email + "',\n\n")
 
+    # TODO add license info
     # License info
     f.write('    # Choose your license\n')
     f.write("    license='MIT',\n\n")
 
     # Classifier info
-    f.write('    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers\n')
+    f.write('    # See https://pypi.python.org/pypi?%3A')
+    f.write('action=list_classifiers\n')
     f.write('    classifiers=[\n')
     f.write('        # How mature is this project? Common values are\n')
     f.write('        #   3 - Alpha\n')
@@ -74,13 +86,17 @@ def write_python(config):
     f.write("        'Intended Audience :: Developers',\n")
     f.write("        'Topic :: Software Development :: Build Tools',\n\n")
 
+    # TODO add license info
     # License Classification info
-    f.write('        # Pick your license as you wish (should match "license" above)\n')
+    f.write('        # Pick your license as you wish (should match')
+    f.write(' "license" above)\n')
     f.write("        'License :: OSI Approved :: MIT License',\n\n")
 
     # Python Version info
-    f.write('        # Specify the Python versions you support here. In particular, ensure\n')
-    f.write('        # that you indicate whether you support Python 2, Python 3 or both.\n')
+    f.write('        # Specify the Python versions you support here.')
+    f.write(' In particular, ensure\n')
+    f.write('        # that you indicate whether you support Python 2,')
+    f.write(' Python 3 or both.\n')
     f.write("        'Programming Language :: Python :: 2',\n")
     f.write("        'Programming Language :: Python :: 2.6',\n")
     f.write("        'Programming Language :: Python :: 2.7',\n")
@@ -96,24 +112,32 @@ def write_python(config):
     f.write("    keywords='sample setuptools development',\n\n")
 
     # Automatic Package inclusion
-    f.write('    # You can just specify the packages manually here if your project is\n')
+    f.write('    # You can just specify the packages manually here if')
+    f.write(' your project is\n')
     f.write('    # simple. Or you can use find_packages().\n')
-    f.write("    packages=find_packages(exclude=['contrib', 'docs', 'tests']),\n\n")
+    f.write("    packages=find_packages(exclude=['contrib', 'docs',")
+    f.write(" 'tests']),\n\n")
 
-    f.write('    # Alternatively, if you want to distribute just a my_module.py, uncomment\n')
+    f.write('    # Alternatively, if you want to distribute just a')
+    f.write(' my_module.py, uncomment\n')
     f.write('    # this:\n')
     f.write('    #   py_modules=["my_module"],\n\n')
 
+    # TODO
     # Run Time dependency
-    f.write('    # List run-time dependencies here.  These will be installed by pip when\n')
-    f.write('''    # your project is installed. For an analysis of "install_requires" vs pip's\n''')
+    f.write('    # List run-time dependencies here.  These will be')
+    f.write(' installed by pip when\n')
+    f.write('    # your project is installed. For an analysis of')
+    f.write(''' "install_requires" vs pip's\n''')
     f.write('    # requirements files see:\n')
     f.write('    # https://packaging.python.org/en/latest/requirements.html\n')
     f.write("    install_requires=['peppercorn'],\n\n")
 
     # Additional dependencies
-    f.write('    # List additional groups of dependencies here (e.g. development\n')
-    f.write('    # dependencies). You can install these using the following syntax,\n')
+    f.write('    # List additional groups of dependencies here (e.g.')
+    f.write(' development\n')
+    f.write('    # dependencies). You can install these using the following')
+    f.write(' syntax,\n')
     f.write('    # for example:\n')
     f.write('    # $ pip install -e .[dev,test]\n')
     f.write('    extras_require={\n')
@@ -122,23 +146,31 @@ def write_python(config):
     f.write('    },\n')
 
     # Required datafiles
-    f.write('    # If there are data files included in your packages that need to be\n')
-    f.write('    # installed, specify them here.  If using Python 2.6 or less, then these\n')
+    f.write('    # If there are data files included in your packages that')
+    f.write(' need to be\n')
+    f.write('    # installed, specify them here.  If using Python 2.6 or')
+    f.write(' less, then these\n')
     f.write('    # have to be included in MANIFEST.in as well.\n')
     f.write('    package_data={\n')
     f.write("        'sample': ['package_data.dat'],\n")
     f.write('    },\n\n')
 
-    f.write("    # Although 'package_data' is the preferred approach, in some case you may\n")
+    f.write("    # Although 'package_data' is the preferred approach, in")
+    f.write(' some case you may\n")
     f.write('    # need to place data files outside of your packages. See:\n')
-    f.write('    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa\n')
-    f.write("    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'\n")
+    f.write('    # http://docs.python.org/3.4/distutils/setupscript.html')
+    f.write('#installing-additional-files # noqa\n')
+    f.write("    # In this case, 'data_file' will be installed into")
+    f.write(" '<sys.prefix>/my_data'\n")
     f.write("    data_files=[('my_data', ['data/data_file'])],\n\n")
 
     # Executable scripts
-    f.write('    # To provide executable scripts, use entry points in preference to the\n')
-    f.write('    # "scripts" keyword. Entry points provide cross-platform support and allow\n')
-    f.write('    # pip to create the appropriate form of executable for the target platform.\n')
+    f.write('    # To provide executable scripts, use entry points in')
+    f.write(' preference to the\n')
+    f.write('    # "scripts" keyword. Entry points provide cross-platform')
+    f.write(' support and allow\n')
+    f.write('    # pip to create the appropriate form of executable for the')
+    f.write(' target platform.\n')
     f.write('    entry_points={\n')
     f.write("        'console_scripts': [\n")
     f.write("            'sample=sample:main',\n")
@@ -148,6 +180,7 @@ def write_python(config):
 
     # Close file after writing
     f.close()
+
 
 def write_config(self):
     pass
